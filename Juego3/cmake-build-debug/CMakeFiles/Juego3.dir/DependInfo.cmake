238d7c92529e76939951c495493c412b0f5a29c8
sonivox <maltez7177@gmail.com>
2019-06-24 22:48:58 -0600

    Vamoss!

---
+++ Juego3/cmake-build-debug/CMakeFiles/Juego3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sonivox/Plantillas/Juegos/Juego3/main.cpp" "/home/sonivox/Plantillas/Juegos/Juego3/cmake-build-debug/CMakeFiles/Juego3.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SOIL"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Juego3/cmake-build-debug/CMakeFiles/Juego3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Juego3.
+# This may be replaced when dependencies are built.
